b50701a2e470cc011b8182c7fa9192258b3ac288
Kamlesh Kumar <kamleshbhalui@gmail.com>
2022-07-20 02:09:48 -0800

    Lift condition for checking 3 constructor args

--- backends/dpdk/dpdkArch.h
+++ backends/dpdk/dpdkArch.h
@@ -676,9 +676,11 @@ class CollectExternDeclaration : public Inspector {
                             "%1%: expected size and optionally init_val as arguments", d);
                 }
             } else if (externTypeName == "MatchValueLookupTable") {
-                if (d->arguments->size() != 3) {
+                if (d->arguments->size() != 1 &&
+                    d->arguments->size() != 2 &&
+                    d->arguments->size() != 3) {
                     ::error(ErrorType::ERR_EXPECTED,
-                            "%1%: requires three arguments arguments", d);
+                            "%1%: requires one or two or three arguments arguments", d);
                 }
             } else {
                 // unsupported extern type
